eaec45ed4d70f378e6369c64dabf5cbbd28430bf
Alex Choi <alex.ji.choi@gmail.com>
2017-11-26 00:02:17 -0500

    Add logout function in urls.py and view.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-		url(r'^$', views.post_home, name='post_home'),	
+	#POST
+	url(r'^$', views.post_home, name='post_home'),	
+	#LOGIN
+	url(r'^logout/$', views.logout_view, name='user_logout'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,15 @@
+from django.contrib.auth import logout
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import render
+from django.urls import reverse
 
 
+'''Main View Page'''
 def post_home(request):
 	return render(request, 'index.html')
+
+
+'''Logout the user'''
+def logout_view(request):
+	logout(request)
+	return HttpResponseRedirect(reverse('blog:post_home'))
